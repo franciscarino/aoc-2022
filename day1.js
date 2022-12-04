b71e03e608c38dc736e63b7ad8f005650af2ce48
Francis Carino <fgcarino@gmail.com>
2022-12-03 19:27:16 -0800

    complete day 1 pt 1

--- day1.js
+++ day1.js
@@ -1,2 +1,28 @@
 'use strict';
 
+import data from "./day1-data";
+
+function countCalories(data) {
+  const caloriesPerElf = [];
+  let calorieCount = 0;
+  for (let i = 0; i < data.length; i++) {
+    if (!data[i]) {
+      caloriesPerElf.push(calorieCount);
+      calorieCount = 0;
+    } else {
+      calorieCount += data[i];
+    }
+  }
+  return caloriesPerElf;
+}
+
+function findMaxCalories(data) {
+  return Math.max(...countCalories(data));
+}
+
+findMaxCalories(data);
+
+// 68292
+
+
+
