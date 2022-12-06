42fe5fc857a161826bcfe12d10887244d177d184
Francis Carino <fgcarino@gmail.com>
2022-12-06 15:38:07 -0800

    add calculateWinOrLoseScore, calculateTotalPlay, calculateTotalScore functions

--- day2.js
+++ day2.js
@@ -14,9 +14,67 @@ function syncReadFile(filename) {
   return contentsArray;
 }
 
-syncReadFile("./day2-data.txt");
+const contents = syncReadFile("./day2-data.txt");
 
 // A=1, B=2, C=3
 // X=1, Y=2, Z=3
+// rock, paper, scissors
 // lost=0, 3=draw, 6=won
+
+const handScores = {
+  a: 1,
+  b: 2,
+  c: 3,
+  x: 1,
+  y: 2,
+  z: 3
+};
+
+function calculateWinOrLoseScore(play) {
+  let opponentHand = play[0];
+  let myHand = play[2];
+
+  // LOSE
+  if (opponentHand === 'A' && myHand === 'Z') return 0;
+  if (opponentHand === 'B' && myHand === 'X') return 0;
+  if (opponentHand === 'C' && myHand === 'Y') return 0;
+
+  // DRAW
+  if (opponentHand === 'A' && myHand === 'X') return 3;
+  if (opponentHand === 'B' && myHand === 'Y') return 3;
+  if (opponentHand === 'C' && myHand === 'Z') return 3;
+
+  // WIN
+  if (opponentHand === 'A' && myHand === 'Y') return 6;
+  if (opponentHand === 'B' && myHand === 'Z') return 6;
+  if (opponentHand === 'C' && myHand === 'X') return 6;
+}
+
+function calculateTotalPlay(play) {
+  const winLoseScore = calculateWinOrLoseScore(play);
+
+  let myHand = play[2].toLowerCase();
+
+  const myHandScore = handScores[myHand];
+
+  console.log("Total Score: ", winLoseScore + myHandScore);
+  return winLoseScore + myHandScore;
+}
+
+
+function calculateTotalScore(playData) {
+  let totalScore = 0;
+
+  for (let i = 0; i < playData.length; i++) {
+    totalScore += calculateTotalPlay[i];
+  }
+  console.log(totalScore);
+  return totalScore;
+}
+
+calculateTotalScore(contents);
+
+
+
+
 
